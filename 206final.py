2343660acb9b26c505939bcaf78a02c3999d9e11
syedaakt <113367460+syedaakt@users.noreply.github.com>
2023-12-06 15:25:55 -0500

    Add files via upload

--- 206final.py
+++ 206final.py
@@ -7,16 +7,31 @@ from bs4 import BeautifulSoup
 import requests
 import pprint
 
-
+# def get_cities():
+#     source_dir = os.path.dirname(__file__)
+#     fullpath = os.path.join(source_dir, f'SI206--FinalProject--CapitalCities2.html')
+#     with open(fullpath, 'r', encoding="utf-8-sig") as htmlfile:
+#         rawsoup = htmlfile.read()
+#         soup = BeautifulSoup(rawsoup, 'html.parser')
+#         countries = soup.find_all('td')
+#         countries_list = []
+#         for coun in countries:
+#             if len(coun.text)!=1:
+#                 countries_list.append(coun.text)
+#         cities_list = []
+#         for each in range(len(countries_list)):
+#             if each%2!=0:
+#                 cities_list.append(countries_list[each])
+#         print(sorted(cities_list))
+#         return sorted(cities_list)
+        
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
 
-
 cur, conn = setUpDatabase('weather')
-
 
 cur.execute("CREATE TABLE IF NOT EXISTS coordinates (city TEXT, lat NUMBER, lon NUMBER)")
 conn.commit()
@@ -24,7 +39,22 @@ conn.commit()
 API_KEY = '466d3d6a8030052dd52e9a49585f562a'
 
 #get terris list of capitals
-cities = ['London', 'Paris', 'Toronto']
+source_dir = os.path.dirname(__file__)
+fullpath = os.path.join(source_dir, f'SI206--FinalProject--CapitalCities2.html')
+with open(fullpath, 'r', encoding="utf-8-sig") as htmlfile:
+    rawsoup = htmlfile.read()
+    soup = BeautifulSoup(rawsoup, 'html.parser')
+    countries = soup.find_all('td')
+    countries_list = []
+    for coun in countries:
+        if len(coun.text)!=1:
+            countries_list.append(coun.text)
+    cities_list = []
+    for each in range(len(countries_list)):
+        if each%2!=0:
+            cities_list.append(countries_list[each])
+
+cities = sorted(cities_list)
 
 city_coordinates = [] 
 
@@ -44,7 +74,7 @@ for row in to_insert:
 
 conn.commit()
 print(city_coordinates)
-for t in city_coordinates:
-    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={t[1]}&lon={t[2]}&appid="6034a57af0a951949dd91ac6907654cd"'
-    re = requests.get(url).json()
-    print(re)
+# for t in city_coordinates:
+#     url = f'https://history.openweathermap.org/data/2.5/history/city?lat={t[1]}&lon={t[2]}&appid="6034a57af0a951949dd91ac6907654cd"'
+#     r = requests.get(url).json()
+#     print(r)
