5c43b56551892b47fefbe62be605f8f8503ff816
Terri Kang <kangty@Terris-MacBook-Pro.local>
2023-12-08 16:55:52 -0500

    Cleaned up code

--- aqi_calc.py
+++ aqi_calc.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import requests
 import pprint
+import numpy as np
+
 
 # Set up database  
 def setUpDatabase():
@@ -26,7 +28,6 @@ def extract_aqi(cur, conn):
     """
     )
     res = cur.fetchall()
-    # print(res)
     all_info = []
     for aqi in res:
         cur.execute(
@@ -38,10 +39,8 @@ def extract_aqi(cur, conn):
         """
         , (aqi)
     )
-        # print(aqi)
         res = cur.fetchall()
         all_info.append(res[0])
-    # print(all_info)
     return all_info
 
 cur, conn = setUpDatabase()
@@ -69,7 +68,6 @@ def extract_cityID(curr, conn):
         )
         res = cur.fetchall()
         city_id.append(res[0])
-    print(city_id)
     return city_id
 
 cur, conn = setUpDatabase()
@@ -103,13 +101,95 @@ def aqi_average():
     aqi_avg_list.append(sum(south_east)/len(south_east))
     aqi_avg_list.append(sum(south_west)/len(south_west))
 
-    print(aqi_avg_list)
     return aqi_avg_list
-    # print(list(sum(north_east)/len(north_east), sum(north_west)/len(north_west), sum(south_east)/len(south_east), sum(south_west)/len(south_west)))
 
-    # print(len(north_east))
-    # print(len(north_west))
-    # print(len(south_east))
-    # print(len(south_west))
 
-aqi_average()
+
+
+def create_barandpie():
+
+# horizontal bar chart
+    x_axis = ['north_east', 'north_west', 'south_east', 'south_west']
+    y_axis = []
+    for avg in aqi_average():
+        y_axis.append(avg)
+
+    plt.figure(figsize = (10,5))
+    plt.subplot (1, 2, 1)
+    plt.bar(x_axis, y_axis)
+    plt.xlabel('Hemisphere')
+    plt.ylabel('Average AQI')
+    
+    plt.title('AQI Levels of Hemispheres in the World')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+# pie chart
+    lvl_1 = 0
+    lvl_2 = 0
+    lvl_3 = 0
+    lvl_4 = 0
+    lvl_5 = 0
+
+    for info in extract_aqi(cur, conn):
+        if info[3] == 1:
+            lvl_1 += 1
+        if info[3] == 2:
+            lvl_2 += 1
+        if info[3] == 3:
+            lvl_3 += 1
+        if info[3] == 4:
+            lvl_4 += 1
+        if info[3] == 5:
+            lvl_5 += 1
+
+    list_lvl = [lvl_1, lvl_2, lvl_3, lvl_4, lvl_5]
+    list_labels = ["AQI: 1", "AQI: 2", "AQI: 3", "AQI: 4", "AQI: 5"]
+    plt.subplot(1,2,2)
+    plt.pie(list_lvl, labels=list_labels, autopct='%1.2f%%')
+    plt.title('Percentage of Cities in Each AQI Level')
+
+    plt.show()
+
+# scatterplot
+def create_scatterplot():
+    plt.figure(figsize=(10,8))
+    city_list = []
+    cities = extract_cityID(cur, conn)
+    for tup in cities:
+        id2 = tup[0]
+        city = tup[1]
+        aqi2 = tup[2]
+
+        south_west = []
+    
+        for info in extract_aqi(cur, conn):
+            id = info[0]
+            lat = info[1]
+            lon = info[2]
+            aqi = info[3]
+            if lat < 0:
+                if lon < 0:
+                    south_west.append(id)
+        for the_id in south_west:
+            if the_id == id2:
+                city_list.append((city, aqi2))
+
+    for plot in city_list:
+        the_city = plot[0]
+        the_aqi = plot[1]
+        
+        plt.subplot()
+        plt.scatter(the_city, the_aqi, color='olive')
+        plt.xticks(rotation=45)
+        plt.title('AQI Levels of Cities in Highest AQI Average Hemisphere (South-West Hemisphere)')
+
+    plt.show()
+
+def main():
+    aqi_average()
+    create_barandpie()
+    create_scatterplot()
+
+if __name__ == '__main__':
+    main()
