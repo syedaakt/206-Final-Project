b8ea03220309662eb9dd64a70783f2d87b0648ca
Terri Kang <kangty@Terris-MacBook-Pro.local>
2023-12-08 13:57:42 -0500

    Calculated the average AQI per hemisphere

--- aqi_calc.py
+++ aqi_calc.py
@@ -27,22 +27,79 @@ def extract_aqi(cur, conn):
     )
     res = cur.fetchall()
     # print(res)
+    all_info = []
     for aqi in res:
         cur.execute(
         """
-        SELECT coordinates.id, coordinates.lat, airQualities.aqi
+        SELECT coordinates.id, coordinates.lat, coordinates.lon, airQualities.aqi
         FROM coordinates
         JOIN airQualities ON coordinates.id = airQualities.id
         WHERE airQualities.id = ?
         """
         , (aqi)
     )
-        print(aqi)
-    res = cur.fetchall()
-    # print(res)
-
-        # join where 
-        # loop through citiies, join (info from multiple tables), where (check if the city corresponds with info), change coordinates into ids instead of ciites
+        # print(aqi)
+        res = cur.fetchall()
+        all_info.append(res[0])
+    # print(all_info)
+    return all_info
 
 cur, conn = setUpDatabase()
 extract_aqi(cur, conn)
+
+# # Get the city and it's corresponding ID
+# def extract_cityID(curr, conn):
+#     cur.execute(
+#         """
+#         SELECT cities.city, airQualities.aqi
+#         FROM cities
+#         JOIN airQualities ON cities.id = airQualities.id
+#         WHERE airQualities.id = ?
+#         """
+#         , 
+#     )
+#     city_id = cur.fetchall()
+#     print(city_id)
+#     return city_id
+
+# cur, conn = setUpDatabase()
+# extract_cityID(cur, conn)
+
+# Calculate the average AQI based on the city's hemisphere
+def aqi_average():
+    north_east = []
+    north_west = []
+    south_east = []
+    south_west = []
+    for info in extract_aqi(cur, conn):
+        id = info[0]
+        lat = info[1]
+        lon = info[2]
+        aqi = info[3]
+        if lat > 0:
+            if lon > 0:
+                north_east.append(aqi)
+            else:
+                north_west.append(aqi)
+        else:
+            if lon > 0:
+                south_east.append(aqi)
+            else:
+                south_west.append(aqi)
+    
+    aqi_avg_list = []
+    aqi_avg_list.append(sum(north_east)/len(north_east))
+    aqi_avg_list.append(sum(north_west)/len(north_west))
+    aqi_avg_list.append(sum(south_east)/len(south_east))
+    aqi_avg_list.append(sum(south_west)/len(south_west))
+
+    print(aqi_avg_list)
+    return aqi_avg_list
+    # print(list(sum(north_east)/len(north_east), sum(north_west)/len(north_west), sum(south_east)/len(south_east), sum(south_west)/len(south_west)))
+
+    # print(len(north_east))
+    # print(len(north_west))
+    # print(len(south_east))
+    # print(len(south_west))
+
+aqi_average()
